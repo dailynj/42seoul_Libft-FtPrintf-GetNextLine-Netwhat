2397c7799027c309fd2d3784d97f44a2e6bbbcee
Najeong Lee <najlee@c1r7s6.42seoul.kr>
2020-12-23 19:44:56 +0900

    finish ft_strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: najlee <najlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 19:16:52 by najlee            #+#    #+#             */
-/*   Updated: 2020/12/23 19:20:29 by najlee           ###   ########.fr       */
+/*   Updated: 2020/12/23 19:43:53 by najlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 char	*ft_strchr(const char *str, int c)
 {
-
+	while (*str != '\0')
+	{
+		if (*str == c)
+			return ((char *)str);
+		++str;
+	}
+	if (c == '\0')
+		return ((char *)str);
+	return (0);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: najlee <najlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/21 17:16:11 by najlee            #+#    #+#             */
-/*   Updated: 2020/12/23 16:21:25 by najlee           ###   ########.fr       */
+/*   Updated: 2020/12/23 19:34:04 by najlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,6 @@ int		ft_memcmp(const void *str1, const void *str2, size_t n);
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strchr(const char *str, int c);
 
 #endif
