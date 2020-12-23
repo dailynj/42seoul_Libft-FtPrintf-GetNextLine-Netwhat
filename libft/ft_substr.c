57ab44e1e60a4ee5ea1e1c4e8654efcd8df807f1
Najeong Lee <leena9901@gmail.com>
2020-12-24 01:48:13 +0900

    finish ft_substr.c

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,12 +6,34 @@
 /*   By: najlee <najlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/24 00:42:38 by najlee            #+#    #+#             */
-/*   Updated: 2020/12/24 01:20:50 by najlee           ###   ########.fr       */
+/*   Updated: 2020/12/24 01:46:30 by najlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char		*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	char	*tmp;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	if (start >= ft_strlen(s))
+	{
+		if (!(tmp = malloc(1)))
+			return (0);
+		tmp[0] = '\0';
+		return (tmp);
+	}
+	if (!(tmp = malloc(len + 1)))
+		return (0);
+	while (i < len && s[start + i])
+	{
+		tmp[i] = s[start + i];
+		i++;
+	}
+	tmp[i] = '\0';
+	return (tmp);
 }
