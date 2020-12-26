141fa16a95d76ab9a17ef70854bbce300d97e1c4
Najeong Lee <leena9901@gmail.com>
2020-12-26 19:54:33 +0900

    finish libft

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: najlee <najlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/26 19:27:42 by najlee            #+#    #+#             */
-/*   Updated: 2020/12/26 19:36:34 by najlee           ###   ########.fr       */
+/*   Updated: 2020/12/26 19:49:07 by najlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 t_list	*ft_lstmap(t_list *list, void *(*f)(void *), void (*del)(void *))
 {
-		
+	t_list	*result;
+	t_list	*temp;
+
+	result = NULL;
+	while (list)
+	{
+		temp = ft_lstnew((*f)(list->content));
+		if (!temp)
+		{
+			ft_lstclear(&result, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&result, temp);
+		temp = temp->next;
+		list = list->next;
+	}
+	return (result);
 }
