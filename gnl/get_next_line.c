1b9d9f7afeacfc68fb3d8249e7b76f2544753042
이나정 <lnjeong@inajeong-ui-MacBookAir.local>
2021-01-12 18:38:34 +0900

    FINAL PRINTF

---
+++ gnl/get_next_line.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: najlee <najlee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/12 18:09:18 by najlee            #+#    #+#             */
+/*   Updated: 2021/01/12 18:38:22 by najlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int			get_next_line(int fd, char **line)
+{
+	char		buf[BUFFER_SIZE + 1];
+	static char	*backup[OPEN_MAX];
+	int		read_size;
+	int		cut;
+
+
+}
---
+++ gnl/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: najlee <najlee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/12 17:58:25 by najlee            #+#    #+#             */
+/*   Updated: 2021/01/12 17:59:52 by najlee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define OPEN_MAX 256
+
+int	get_next_line(int fd, char **line);
+
+#endif
